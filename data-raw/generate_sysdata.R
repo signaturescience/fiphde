7dc095156b711e0b3ff0ca0253c16f706dcce48a
Stephen Turner <vustephen@gmail.com>
2021-12-23 13:10:08 -0500

    quantiles

--- data-raw/generate_sysdata.R
+++ data-raw/generate_sysdata.R
@@ -21,12 +21,12 @@ locations <-
 
 # Quantiles ---------------------------------------------------------------
 
-# # quantiles needed
-# q <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
+# quantiles needed
+q <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
 # # Figure out what the interval you need to get those quantiles
 # qi <-
 #   tibble(lower=q[q<.5], upper=rev(q[q>.5])) %>%
-#   mutate(interval=round((upper-lower)*100))
+#   mutate(interval=as.integer(round((upper-lower)*100)))
 # qi
 # # The quidk (say: "quiddick") tibble: QUantile, Interval, Direction, Key
 # quidk <-
