60075952763f0468ac9c32c830fb84c92eccc858
Stephen Turner <vustephen@gmail.com>
2021-12-23 13:10:33 -0500

    change week to epiweek, cut out interpolation

--- data-raw/generate_sysdata.R
+++ data-raw/generate_sysdata.R
@@ -42,59 +42,69 @@ q <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
 # Historical severity -----------------------------------------------------
 
 # Get weighted and unweighted ILI (2010-2019), summarize by epiweek
-ili <- cdcfluview::ilinet()
+ili <- cdcfluview::ilinet(region="national")
 ili
 ilisum <-
   ili %>%
   filter(year %>% between(2010, 2019)) %>%
   group_by(week) %>%
-  summarize(wili=mean(weighted_ili), uili=mean(unweighted_ili))
+  summarize(ili=mean(unweighted_ili)) %>%
+  rename(epiweek=week) %>%
+  mutate(ili_rank=rank(-ili) %>% as.integer())
 ilisum
 
 # Get historical hospitalization data (2010-2019), summarize by epiweek
-hosp <- cdcfluview::hospitalizations()
+hosp <- cdcfluview::hospitalizations(surveillance_area = "flusurv", region="all")
 hosp
 hospsum <-
   hosp %>%
   filter(year %>% between(2010, 2019)) %>%
   filter(age_label=="Overall") %>%
-  rename(week=year_wk_num) %>%
-  group_by(week) %>%
-  summarize(hosp=mean(weeklyrate))
+  rename(epiweek=year_wk_num) %>%
+  group_by(epiweek) %>%
+  summarize(hosp=mean(weeklyrate)) %>%
+  mutate(hosp_rank=rank(-hosp) %>% as.integer())
 hospsum
 
 # join ili and hosp summaries
 ih <-
-  left_join(ilisum, hospsum, by="week") %>%
+  left_join(ilisum, hospsum, by="epiweek") %>%
   mutate(across(everything(), replace_na, 0)) %>%
-  mutate(day=(week-1)*7+1, .after=week)
+  mutate(day=(epiweek-1)*7+1, .after=epiweek)
 ih
 
-# interpolate (see https://github.com/signaturescience/fluforce-init/blob/f8b985c0c6eb5b96bcf32f9f7a568e0265ec50c6/ilinet-epiweek.R#L47-L54)
-# first create an approximation function based on day (x) and value (y) in the joined data above.
-# run that function on the day variable in this tibble, which runs 1:365
-# left join the original data back in and add which ones are interpolated
-historical_severity <-
-  tibble(day=1:max(ih$day)) %>%
-  mutate(wili=approxfun(x=ih$day, y=ih$wili)(day)) %>%
-  mutate(uili=approxfun(x=ih$day, y=ih$uili)(day)) %>%
-  mutate(hosp=approxfun(x=ih$day, y=ih$hosp)(day)) %>%
-  left_join(ih) %>%
-  relocate(week) %>%
-  mutate(interpolated=is.na(week), .after=day) %>%
-  tidyr::fill(week, .direction="down")
-historical_severity
+# # interpolate (see https://github.com/signaturescience/fluforce-init/blob/f8b985c0c6eb5b96bcf32f9f7a568e0265ec50c6/ilinet-epiweek.R#L47-L54)
+# # first create an approximation function based on day (x) and value (y) in the joined data above.
+# # run that function on the day variable in this tibble, which runs 1:365
+# # left join the original data back in and add which ones are interpolated
+# historical_severity <-
+#   tibble(day=1:max(ih$day)) %>%
+#   mutate(wili=approxfun(x=ih$day, y=ih$wili)(day)) %>%
+#   mutate(uili=approxfun(x=ih$day, y=ih$uili)(day)) %>%
+#   mutate(hosp=approxfun(x=ih$day, y=ih$hosp)(day)) %>%
+#   left_join(ih) %>%
+#   relocate(epiweek) %>%
+#   mutate(interpolated=is.na(epiweek), .after=day) %>%
+#   tidyr::fill(epiweek, .direction="down")
+# historical_severity
+#
+# historical_severity_tidy <-
+#   historical_severity %>%
+#   gather(metric, value, wili, uili, hosp)
+# ggplot(ihitidy, aes(day, value)) +
+#   geom_line(lwd=.2) +
+#   geom_point(size=.5) +
+#   geom_point(data=filter(historical_severity_tidy, !interpolated), aes(day, value), col="red") +
+#   facet_wrap(~metric, scale="free_y", ncol=1)
+# rm(historical_severity_tidy)
 
-historical_severity_tidy <-
-  historical_severity %>%
-  gather(metric, value, wili, uili, hosp)
-ggplot(ihitidy, aes(day, value)) +
-  geom_line(lwd=.2) +
-  geom_point(size=.5) +
-  geom_point(data=filter(historical_severity_tidy, !interpolated), aes(day, value), col="red") +
-  facet_wrap(~metric, scale="free_y", ncol=1)
-rm(historical_severity_tidy)
+# Skip all the interpolation stuff
+historical_severity <- ih %>% select(-day)
+
+historical_severity %>%
+  gather(key, value, ili, hosp) %>%
+  ggplot(aes(epiweek, value)) + facet_wrap(~key) + geom_point()
 
 # Write package data ------------------------------------------------------
 
-usethis::use_data(locations, historical_severity, internal = TRUE, overwrite = TRUE)
+usethis::use_data(locations, q, historical_severity, internal = TRUE, overwrite = TRUE)
