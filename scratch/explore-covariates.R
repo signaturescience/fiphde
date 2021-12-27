2870e336cad453664862250f544b4e53191d79ec
Stephen Turner <vustephen@gmail.com>
2021-12-27 08:45:33 -0500

    more exploration of ili, flu.admits

--- scratch/explore-covariates.R
+++ scratch/explore-covariates.R
@@ -1,20 +1,24 @@
 library(tidyverse)
 library(fiphde)
+theme_set(theme_bw())
 # library(fable)
 
 # Get ili data from 2010 forward
 i <- get_cdc_ili(region=c("national", "state"), years=2010:lubridate::year(lubridate::today()))
+i
 
-# US only
+# US only ILI data
 iu <-
   i %>%
   filter(location=="US") %>%
   select(location, epiyear, epiweek, week_start, ili=unweighted_ili)
-iu %>% tail()
+iu
+tail(iu)
 
-# Get hosp data
+# Get hosp data from healthdata.gov
 h <- get_hdgov_hosp()
 h
+tail(h)
 
 # weekly data summed across states
 hu <-
@@ -28,12 +32,74 @@ hu <-
   mutate(location = "US", .before = "epiyear")
 hu
 
-hu %>%
+# join hospitalization data to ili data
+hi <-
+  hu %>%
   left_join(iu, by = c("location", "epiyear", "epiweek")) %>%
-  inner_join(fiphde:::historical_severity, by=c("epiweek"))
+  inner_join(fiphde:::historical_severity, by=c("epiweek")) %>%
+  mutate(week_start=MMWRweek::MMWRweek2Date(epiyear, epiweek))
+tail(hi)
+
+# How many weeks of ili data are you missing?
+nmiss_ili <- sum(is.na(hi$ili))
+message(nmiss_ili)
+
+# lag by nmiss_ili weeks
+hi <- hi %>%
+  arrange(week_start) %>%
+  mutate(ililag=lag(ili, n = nmiss_ili), .after=ili) %>%
+  mutate(ililag=ifelse(is.na(ililag), first(ili), ililag))
+tail(hi)
+head(hi)
+
+# hospitalization data doesn't really pick up until flu season 2020-2021
+ggplot(hi, aes(week_start, flu.admits)) + geom_point()
+h %>% filter(flu.admits>1)
+hi <-
+  hi %>%
+  filter(epiyear>=2020 & epiweek>=42)
+ggplot(hi, aes(week_start, flu.admits)) + geom_point()
 
 
-ilidat_us %>%
-  make_tsibble(epiyear=epiyear, epiweek=epiweek, key=location)
+library(ggpubr)
+scatter <- function(.data, .x, .y) {
+  ggscatter(.data, x=.x, y=.y,
+            add="reg.line",
+            add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+            conf.int=TRUE,
+            cor.coef=TRUE,
+            cor.coeff.args = list(method = "pearson", label.x = 1, label.sep = "\n"))
+}
 
-fit <- model(ilidat_us, arima=ARIMA(ili), ets=ETS(ili))
+hi %>%
+  scatter("ili", "flu.admits")
+hi %>%
+  scatter("ililag", "flu.admits")
+
+hi %>%
+  scatter("flu.admits.cov", "flu.admits")
+
+hi %>%
+  mutate(lag=lag(flu.admits, 1)) %>%
+  scatter("lag", "flu.admits")
+hi %>%
+  mutate(lag=lag(flu.admits, 2)) %>%
+  scatter("lag", "flu.admits")
+hi %>%
+  mutate(lag=lag(flu.admits, 3)) %>%
+  scatter("lag", "flu.admits")
+hi %>%
+  mutate(lag=lag(flu.admits, 4)) %>%
+  scatter("lag", "flu.admits")
+hi %>%
+  mutate(lag=lag(flu.admits, 5)) %>%
+  scatter("lag", "flu.admits")
+
+hi %>%
+  scatter("hosp_mean", "flu.admits")
+hi %>%
+  scatter("hosp_rank", "flu.admits")
+hi %>%
+  scatter("ili_mean", "flu.admits")
+hi %>%
+  scatter("ili_rank", "flu.admits")
