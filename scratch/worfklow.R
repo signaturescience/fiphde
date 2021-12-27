c82b5dd798a0b10978b9a980a3124b8740045ad6
Stephen Turner <vustephen@gmail.com>
2021-12-27 12:39:25 -0500

    updating workflow to use ili and hosp ranks

--- scratch/worfklow.R
+++ scratch/worfklow.R
@@ -33,7 +33,7 @@ p.ili <-
   labs(x="Date", y="Unweighted ILI", title="ILI forecast")
 p.ili
 
-hosp <- get_hdgov_hosp(mindate="2021-04-01", maxdate="2021-12-12")
+hosp <- get_hdgov_hosp(mindate="2021-04-01", maxdate="2021-12-25")
 
 tmp_weekly_flu <-
   hosp %>%
@@ -52,35 +52,44 @@ tmp_weekly_flu <-
 ## add lag columns
 tmp_weekly_flu_w_lag <-
   tmp_weekly_flu %>%
-  mutate(lag_1 = lag(flu.admits, 1),
-         lag_2 = lag(flu.admits, 2),
-         lag_3 = lag(flu.admits, 3),
-         lag_4 = lag(flu.admits, 4)) %>%
+  mutate(lag_1 = lag(flu.admits, 1)) %>%
   filter(complete.cases(.)) %>%
   dplyr::mutate(date = MMWRweek::MMWRweek2Date(epiyear, epiweek)) %>%
+  inner_join(fiphde:::historical_severity, by="epiweek") %>%
   filter(date >= max(date) - 7*24)
+  print()
+tmp_weekly_flu_w_lag
 
 train_dat <- tmp_weekly_flu_w_lag %>% filter(row_number() < n() - 3)
 test_dat <- tmp_weekly_flu_w_lag %>% filter(row_number() >= n() - 3)
 
 models <-
   list(
-    glm_poisson_ili = trending::glm_model(flu.admits ~ ili, family = "poisson"),
-    glm_quasipoisson_ili = trending::glm_model(flu.admits ~ ili, family = "quasipoisson"),
-    glm_negbin_ili = trending::glm_nb_model(flu.admits ~ ili),
-    glm_poisson_ili_offset = trending::glm_model(flu.admits ~ ili + offset(flu.admits.cov), family = "poisson"),
-    glm_quasipoisson_ili_offset = trending::glm_model(flu.admits ~ ili + offset(flu.admits.cov), family = "quasipoisson"),
-    glm_negbin_ili_offset = trending::glm_nb_model(flu.admits ~ ili + offset(flu.admits.cov)),
-    glm_poisson_ili_lags = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4, family = "poisson"),
-    glm_quasipoisson_ili_lags = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4, family = "quasipoisson"),
-    glm_negbin_ili_lags = trending::glm_nb_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4),
-    glm_poisson_ili_lags_offset = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4 + offset(flu.admits.cov), family = "poisson"),
-    glm_quasipoisson_ili_lags_offset = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4 + offset(flu.admits.cov), family = "quasipoisson"),
-    glm_negbin_ili_lags_offset = trending::glm_nb_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4 + offset(flu.admits.cov))
+    # glm_poisson_ili = trending::glm_model(flu.admits ~ ili, family = "poisson"),
+    # glm_quasipoisson_ili = trending::glm_model(flu.admits ~ ili, family = "quasipoisson"),
+    # glm_negbin_ili = trending::glm_nb_model(flu.admits ~ ili),
+    # glm_poisson_ili_offset = trending::glm_model(flu.admits ~ ili + offset(flu.admits.cov), family = "poisson"),
+    # glm_quasipoisson_ili_offset = trending::glm_model(flu.admits ~ ili + offset(flu.admits.cov), family = "quasipoisson"),
+    # glm_negbin_ili_offset = trending::glm_nb_model(flu.admits ~ ili + offset(flu.admits.cov)),
+    # glm_poisson_ili_lags = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4, family = "poisson"),
+    # glm_quasipoisson_ili_lags = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4, family = "quasipoisson"),
+    # glm_negbin_lags_means = trending::glm_nb_model(flu.admits ~ lag_1 + ili_mean + hosp_mean),
+    glm_negbin_lags_ranks = trending::glm_nb_model(flu.admits ~ lag_1 + ili_rank + hosp_rank),
+    glm_negbin_ili_lags_ranks = trending::glm_nb_model(flu.admits ~ ili + lag_1 + ili_rank + hosp_rank),
+    glm_negbin_ili_lags_ranks_offset = trending::glm_nb_model(flu.admits ~ ili + lag_1 + ili_rank + hosp_rank + offset(flu.admits.cov))
+    # glm_negbin_ili_lags = trending::glm_nb_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4),
+    # glm_poisson_ili_lags_offset = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4 + offset(flu.admits.cov), family = "poisson"),
+    # glm_quasipoisson_ili_lags_offset = trending::glm_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4 + offset(flu.admits.cov), family = "quasipoisson"),
+    # glm_negbin_ili_lags_offset = trending::glm_nb_model(flu.admits ~ ili + lag_1 + lag_2 + lag_3 + lag_4 + offset(flu.admits.cov))
   )
 
 res <- glm_wrap(train_dat,
-                new_covariates = tibble(flu.admits.cov = rep(tail(train_dat$flu.admits.cov,1), 4), ili = ilifor$ili_future$ili),
+                new_covariates = tibble(flu.admits.cov = rep(tail(train_dat$flu.admits.cov,1), 4),
+                                        ili_rank=test_dat$ili_rank,
+                                        hosp_rank=test_dat$hosp_rank,
+                                        ili_mean=test_dat$ili_mean,
+                                        hosp_mean=test_dat$hosp_mean,
+                                        ili=test_dat$ili),
                 .models = models,
                 alpha = c(0.01, 0.025, seq(0.05, 0.45, by = 0.05)) * 2)
 
