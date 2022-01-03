f21a3fd4b34d31fda3121dabfd76afbc7b2ea51f
Stephen Turner <vustephen@gmail.com>
2022-01-03 15:31:35 -0500

    function to format ts for submission (note not bounded) #10 #37

--- R/fiphde-package.R
+++ R/fiphde-package.R
@@ -85,5 +85,12 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(".",
                                                         "region",
                                                         "weighted_ili_now",
                                                         "ets",
+                                                        ".model",
+                                                        "key",
+                                                        "N",
+                                                        "forecast_date",
+                                                        "target",
+                                                        "target_end_date",
+                                                        "type",
                                                         "."))
 
--- R/forecast.R
+++ R/forecast.R
@@ -294,3 +294,95 @@ forecast_ili <- function(ilidat, horizon=4L, trim_date=NULL, type="arima", const
   res <- tibble::lst(ilidat, ilidat_tsibble, ili_fit, ili_forecast, ili_future, ili_bound, arima_params, locstats, removed)
   return(res)
 }
+
+
+#' @title Format time series forecast
+#' @description Format time series forecast for submission.
+#' @details Uses quantiles `c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)` in the built-in `fiphde:::q`, using an accessory table `fiphde:::quidk`. See `data-raw/generate-sysdata.R` for details.
+#' @param tsfor The forecast from [ts_fit_forecast].
+#' @return A named list of tibbles, one for each model, formatted for submission.
+#' @references <https://github.com/cdcepi/Flusight-forecast-data/blob/master/data-forecasts/README.md>
+#' @export
+#' @examples
+#' \dontrun{
+#' # Get raw data from healthdata.gov
+#' h_raw <- get_hdgov_hosp(limitcols=TRUE)
+#' ## save(h_raw, file="~/Downloads/h_raw.rd")
+#' ## load(file="~/Downloads/h_raw.rd")
+#'
+#' # Prep, and make a tsibble
+#' prepped_hosp <- prep_hdgov_hosp(h_raw, statesonly=TRUE)
+#' prepped_hosp_tsibble <- make_tsibble(prepped_hosp,
+#'                                      epiyear = epiyear,
+#'                                      epiweek=epiweek,
+#'                                      key=location)
+#' # Limit to only Virginia and US
+#' prepped_hosp_tsibble <-
+#'   prepped_hosp_tsibble %>%
+#'   dplyr::filter(location %in% c("US", "51"))
+#'
+#' # Fit a model
+#' hosp_fitfor <- ts_fit_forecast(prepped_hosp_tsibble,
+#'                                horizon=4L,
+#'                                outcome="flu.admits",
+#'                                constrained=TRUE,
+#'                                covariates=c("hosp_rank", "ili_rank"))
+#'
+#' # format for submission
+#' formatted_list <- ts_format_for_submission(hosp_fitfor$tsfor)
+#' formatted_list
+#' }
+ts_format_for_submission <- function (tsfor) {
+  # Make the 0.5 quantile the means (point estimates). The quidk doesn't contain a
+  # median hilo. You'll bind this to the other quantiles in the step below.
+  q5 <-
+    tsfor %>%
+    tibble::as_tibble() %>%
+    dplyr::transmute(.model, yweek, location, quantile=0.5, value=.mean, type="quantile") %>%
+    dplyr::arrange(.model, yweek)
+
+  # Get the point estimates
+  point_estimates <-
+    tsfor %>%
+    dplyr::as_tibble() %>%
+    dplyr::mutate(quantile=NA_real_, .after=yweek) %>%
+    dplyr::mutate(type="point") %>%
+    dplyr::rename(value=.mean) %>%
+    dplyr::select(.model, yweek, location, quantile, value, type) %>%
+    dplyr::arrange(.model, yweek)
+
+  # Create quantile table from distribution column in the forecast
+  quantiles <-
+    tsfor %>%
+    fabletools::hilo(as.double(sort(unique(quidk$interval)))) %>%
+    fabletools::unpack_hilo(dplyr::ends_with("%")) %>%
+    tidyr::gather(key, value, dplyr::contains("%")) %>%
+    dplyr::inner_join(quidk, by="key") %>%
+    tibble::as_tibble() %>%
+    dplyr::transmute(.model, yweek, location, quantile, value, type="quantile") %>%
+    dplyr::arrange(.model, yweek, quantile)
+
+  # bind them all together
+  submission_list <-
+    list(quantiles, point_estimates, quantiles) %>%
+    purrr::reduce(dplyr::bind_rows) %>%
+    dplyr::select(.model:type) %>%
+    dplyr::arrange(.model, location, type, quantile, yweek) %>%
+    ## processing to get horizon N
+    dplyr::group_by(.model, yweek) %>%
+    dplyr::mutate(N=dplyr::cur_group_id()) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(target=sprintf("%d wk ahead inc flu hosp", N)) %>%
+    dplyr::select(-N) %>%
+    # Fix dates: as_date(yweek) returns the MONDAY that starts that week. add 5 days to get the Saturday date.
+    dplyr::mutate(target_end_date=lubridate::as_date(yweek)+lubridate::days(5)) %>%
+    dplyr::mutate(forecast_date=lubridate::today()) %>%
+    dplyr::select(.model, forecast_date, target, target_end_date, location, type, quantile, value) %>%
+    # split by the type of model
+    split(.$.model) %>%
+    # remove the .model variable from each list item
+    purrr::map(dplyr::select, -.model)
+
+  return(submission_list)
+
+}
