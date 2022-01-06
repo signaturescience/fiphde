cfc0fd6d01c59782f80b619a798d788cc19db4ae
VP Nagraj <pnagraj@signaturescience.com>
2022-01-06 18:22:38 -0500

    adding wrapper for formatting either glm or ts forecasts

--- R/fiphde-package.R
+++ R/fiphde-package.R
@@ -93,5 +93,6 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(".",
                                                         "target_end_date",
                                                         "type",
                                                         "point",
+                                                        "horizon",
                                                         "."))
 
