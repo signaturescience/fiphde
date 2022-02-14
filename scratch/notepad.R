04f4fbe923655053c4731a5387b22ccdc8f8a71d
shakeeljessa <shakeel@utexas.edu>
2022-02-14 08:09:40 -0600

    move app

--- scratch/notepad.R
+++ scratch/notepad.R
@@ -16,6 +16,15 @@ data_dir <- "C:/Users/sjessa/OneDrive - Signature Science, LLC/Documents/FIPDHE/
 fps <- rev(list.files(data_dir, pattern = "*.csv$",
                       recursive = TRUE, full.names = TRUE))
 
+read_forc <- function(fp) {
+  tmp <- read_csv(fp)
+  tmp %>%
+    mutate(filename = basename(fp)) %>%
+    mutate(model = basename(dirname(fp)))
+}
+
+sub_forecasts <- map_df(fps, read_forc)
+
 # inspect each cvs for debugging
 # a <- read_csv(fps[1])
 # b <- read_csv(fps[2])
@@ -34,6 +43,6 @@ dates <- unique(str_extract(fps, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
 
 sub_loc <- "04"
 sub_date <- dates[1]
-sub_forecasts <- all_forecasts %>% filter(forecast_date == sub_date)
+sub_forecasts <- sub_forecasts %>% filter(forecast_date == sub_date)
 
 plot_forecast(prepped_hosp, sub_forecasts, sub_loc)
