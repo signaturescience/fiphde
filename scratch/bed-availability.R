0bba09dd40fef63355a4461f242deb65295c36ba
Stephen Turner <vustephen@gmail.com>
2022-03-17 09:41:52 -0400

    flu admits

--- scratch/bed-availability.R
+++ scratch/bed-availability.R
@@ -7,9 +7,9 @@ h
 hs <- h %>%
   filter(date>="2021-10-03") %>%
   select(state, date,
-         flu=flu.admits,
+         flu.admits,
          # flu.cov=flu.admits.cov,
-         covid=cov.admits,
+         cov.admits,
          # covid.cov=cov.admits.cov,
          beds=inpatient_beds, beds.used=inpatient_beds_used,
          beds.used.covid=inpatient_beds_used_covid) %>%
@@ -30,10 +30,10 @@ hs %>%
 
 hs %>%
   select(-epiyear, -epiweek) %>%
-  gather(key, value, flu, beds.used.covid) %>%
+  gather(key, value, flu.admits, beds.used.covid) %>%
   ggplot(aes(date, value)) + geom_line() + geom_point() +
   facet_wrap(~key, scale="free_y", ncol=1) +
-  geom_vline(xintercept=hs$date[which(hs$flu==max(hs$flu))], lty=2, col="blue2") +
+  geom_vline(xintercept=hs$date[which(hs$flu.admits==max(hs$flu.admits))], lty=2, col="blue2") +
   geom_vline(xintercept=hs$date[which(hs$beds.used.covid==max(hs$beds.used.covid))], lty=2, col="red2")
 
 hs %>%
