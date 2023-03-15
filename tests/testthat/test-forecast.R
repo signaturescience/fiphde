b2b28e945191dc14c635e2200735f045f0884ffc
VP Nagraj <pnagraj@signaturescience.com>
2023-03-15 18:30:08 -0400

    categorical forecast test

--- tests/testthat/test-forecast.R
+++ tests/testthat/test-forecast.R
@@ -16,3 +16,25 @@ test_that("Time series modeling and forecasting works as expected", {
   })
 
 })
+
+test_that("Categorical forecasting operates as expected", {
+
+  set.seed(2023-01-30)
+  hosp_fitfor_test <- ts_fit_forecast(prepped_tsibble, horizon=4L, outcome="flu.admits", covariates=TRUE)
+  formatted_list <- format_for_submission(hosp_fitfor_test$tsfor, method = "ts")
+  ens_forc <- formatted_list$ensemble
+  cat_forcs <- forecast_categorical(ens_forc, prepped_hosp)
+
+  ## check that categorical forecasts include all type_id values
+  expect_equal(sort(unique(as.character(cat_forcs$type_id))), c("decrease","increase","large_decrease","large_increase","stable"))
+
+  ## check that categorical forecasts all type id values are represented in every location
+  n_locs <- length(unique(ens_forc$location))
+  type_id_counts <-
+    cat_forcs %>%
+    dplyr::count(type_id) %>%
+    dplyr::pull(n)
+
+  expect_true(all(type_id_counts == n_locs))
+
+})
