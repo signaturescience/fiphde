bb1e6eb3b1b1e635bbcaab0fed82309448be8105
VP Nagraj <pnagraj@signaturescience.com>
2024-12-05 15:37:03 -0500

    adds prep function for nhsn weekly data

--- R/prep.R
+++ R/prep.R
@@ -173,3 +173,101 @@ make_tsibble <- function(df, epiyear, epiweek, key=location) {
     tsibble::as_tsibble(index=yweek, key={{key}})
   return(out)
 }
+
+#' @title Prepare NHSN weekly data
+#'
+#' @description This function prepares data retrieved from the weekly aggregated NHSN hospital respiratory data API. The data must be first retrieved using [prep_nhsn_weekly]. Once pulled from the API, this function will conditionally adjust partial reporting and add extended time series data (see 'Details' for more information). The preparation also includes joining to internal data prepared to estimate the historical severity of each epiweek.
+#'
+#' @param dat Weekly hospital utilization data from [prep_nhsn_weekly]
+#' @param adjust_partial Logical as to whether or not the partial reporting should be adjusted (see 'Details' for more); default is `TRUE`.
+#' @param statesonly Logical as to whether or not the data should be limited to states and DC (i.e., no other territories included); default is `TRUE`.
+#' @param augment Logical as to whether or not the data should be augmented with NHSN hospitalizations imputed backwards in time (see 'Details' for more); default is `FALSE`.
+#' @param augment_stop Date at which the time series imputation data should stop; yyyy-mm-dd format; only used if "augment" is `TRUE` default is `"2020-10-18"`
+#'
+#' @details
+#'
+#' The weekly aggregated data from NHSN includes locations that may have incomplete coverage of hospitals reporting (see <https://data.cdc.gov/Public-Health-Surveillance/Weekly-Hospital-Respiratory-Data-HRD-Metrics-by-Ju/mpgq-jmmr/about_data> for more information). The preparation in this function includes an optional step triggered by the "adjust_partial" argument to find the maximum coverage at any time point for each location, then adjusts the reported counts by a factor of X / Y_t, where X is the maximum coverage and Y_t is the coverage at time point t. If the coverage for the given week is near or equal to the maximum observed coverage, then the counts will have little to no effect on the counts. Note that this should be used with caution, as it is possible that some locations may have non-uniform reporting behaviors, especially during non-mandatory NHSN reporting windows. In other words, the counts may be adjusted using reported values from healthcare facilities that may be of a different size, serve different communities, or otherwise have different characteristics than the facilities that did not report.
+#'
+#' The preparation for the weekly flu hospitalization data includes an option to "augment" the input time series. The augmentation is based on an extended time series that was developed with an imputation approach. The extended time series estimates flu hospitalizations at the state-level in years before NHSN reporting became available. If the user decides to include the imputed data, then the time series is extended backwards in time from the "augment_stop" date (defaults to October 18, 2020). The prepended data augmentation is formatted to match the true NSHN reporting. For more details on the data augmentation approach, refer to the publication: <https://www.medrxiv.org/content/10.1101/2024.07.31.24311314v1>.
+#'
+#' @return A `tibble` with hospitalization data summarized to epiyear/epiweek with the following columns:
+#'
+#' - **abbreviation**: Abbreviation for the location
+#' - **location**: FIPS code for the location
+#' - **week_start**: Date of beginning (Sunday) of the given epidemiological week
+#' - **monday**: Date of Monday of the given epidemiological week
+#' - **week_end**: Date of end (Saturday) of the given epidemiological week
+#' - **epiyear**: Year of reporting (in epidemiological week calendar)
+#' - **epiweek**: Week of reporting (in epidemiological week calendar)
+#' - **flu.admits**: Count of flu cases among admitted patients on previous day
+#' - **flu.admits.cov**: Coverage (number of hospitals reporting) for incident flu cases
+#' - **ili_mean**: Estimate of historical ILI activity for the given epidemiological week
+#' - **ili_rank**: Rank of the given epidemiological week in terms of ILI activity across season (1 being highest average activity)
+#' - **hosp_mean**: Estimate of historical flu hospitalization rate for the given epidemiological week
+#' - **hosp_rank**: Rank of the given epidemiological week in terms of flu hospitalizations across season (1 being highest average activity)
+#'
+prep_nhsn_weekly <- function(dat,
+                             adjust_partial = TRUE,
+                             statesonly = TRUE,
+                             augment = FALSE,
+                             augment_stop = "2020-10-18") {
+
+  ## fix "USA" abbreviation to be consistent with fiphde:::locations "US"
+  dat <-
+    dat %>%
+    dplyr::mutate(abbreviation = ifelse(abbreviation == "USA", "US", abbreviation))
+
+  if(adjust_partial) {
+    dat <-
+      dat %>%
+      dplyr::group_by(abbreviation) %>%
+      ## find the max reporting percentage and adjust up to that
+      dplyr::mutate(max_reporting = max(flu.admits.cov.perc, na.rm = TRUE)) %>%
+      dplyr::ungroup() %>%
+      ## TODO: double check this logic
+      ## performs the adjustment
+      dplyr::mutate(flu.admits = round((max_reporting/flu.admits.cov.perc)*flu.admits))
+  } else {
+    dat <-
+      dat %>%
+      dplyr::mutate(flu.admits = round(flu.admits))
+  }
+
+  ## if the augment option is set then prepend with the imputed data
+  if(augment) {
+    dat <-
+      ## start with internal imputation object
+      nhsn_imputed %>%
+      dplyr::select(abbreviation = location, week_start = date, flu.admits = mean_flu_admits) %>%
+      ## NOTE: augmentation includes data that extends beyond the date we have used to trim HHS data previously
+      ## this line (in combination with filter in the next line) ...
+      ## ... will ensure that the data pulled from the NHSN API is used for all dates after augment_stop date
+      dplyr::mutate(week_end = week_start + 6) %>%
+      dplyr::select(-week_start) %>%
+      dplyr::filter(week_end <= as.Date(augment_stop)) %>%
+      dplyr::bind_rows(dplyr::filter(dat, week_end > as.Date(augment_stop)),.) %>%
+      dplyr::arrange(abbreviation, week_end)
+  }
+
+
+  dat <-
+    dat %>%
+    dplyr::select(abbreviation, flu.admits, week_end, flu.admits.cov) %>%
+    dplyr::left_join(locations) %>%
+    dplyr::mutate(epiyear=lubridate::epiyear(week_end), epiweek=lubridate::epiweek(week_end)) %>%
+    dplyr::mutate(week_start = mmwr_week_to_date(epiyear, epiweek, 1)) %>%
+    dplyr::mutate(monday = mmwr_week_to_date(epiyear, epiweek, 2)) %>%
+    dplyr::left_join(historical_severity, by="epiweek")
+
+  # Trim to US+states+DC only
+  if(statesonly) {
+    message("Filtering to US+DC+States only")
+    dat <-
+      dat %>%
+      dplyr::filter(location %in% c("US", stringr::str_pad(1:56, width=2, pad="0")))
+  }
+
+  dat %>%
+    select(abbreviation, location, week_start, monday, week_end, epiyear, epiweek, flu.admits, flu.admits.cov, ili_mean, ili_rank, hosp_mean, hosp_rank)
+
+}
